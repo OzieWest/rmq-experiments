d0708a862ff5fa7d2fea53575ea89c6b1b8288d3
SoraMusoka <soramusoka@gmail.com>
2014-01-29 00:57:09 +0400

    Стартовай проект, подключены базовые зависимости, убрано все лишнее.

---
+++ RMQ Simple project/App_Start/AuthConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using RMQ_Simple_project.Models;
+
+namespace RMQ_Simple_project
+{
+	public static class AuthConfig
+	{
+		public static void RegisterAuth()
+		{
+			//OAuthWebSecurity.RegisterMicrosoftClient(
+			//    clientId: "",
+			//    clientSecret: "");
+
+			//OAuthWebSecurity.RegisterTwitterClient(
+			//    consumerKey: "",
+			//    consumerSecret: "");
+
+			//OAuthWebSecurity.RegisterFacebookClient(
+			//    appId: "",
+			//    appSecret: "");
+
+			OAuthWebSecurity.RegisterGoogleClient();
+		}
+	}
+}
---
+++ RMQ Simple project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RMQ_Simple_project.MvcApplication" Language="C#" %>
